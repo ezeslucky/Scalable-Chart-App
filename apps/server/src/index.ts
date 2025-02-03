0c8b401fc16f98ab9236c0c3578ac82b95518ed9
Lucky <lucky123@gmail.com>
2025-02-03 23:01:13 +0530

    inital socket.io

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,15 @@
 import http from "http"
+import SocketService from "./services/socket"
 
 async function init() {
+    const socketService = new SocketService()
     const httpServer = http.createServer()
-    const PORT = process.env.PORT ? process.env.PORTv: 8000
 
+
+    const PORT = process.env.PORT ? process.env.PORT: 8000
+
+    socketService.io.attach(httpServer)
     httpServer.listen(PORT, () => console.log(`HTTP Server at Port: ${PORT}`))
-    
+    socketService.initListeners()
 }
-init()
+init() 
